6be9999fd9fbcf717f5ba7636cb1a04636d1c0d4
jawad68155 <jawad68155@gmail.com>
2024-06-15 13:04:33 +0500

    fetch api

---
+++ json/assets/js/script.js
@@ -0,0 +1,20 @@
+fetch('/json/assets/json/data.json')
+    .then(response => response.json())
+    .then(data => {
+        let x = '';
+        for (let keys of data) {
+            // console.log(keys);
+            x += `
+                <div class="col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="${keys.image}" alt="Title" />
+                        <div class="card-body">
+                            <h4 class="card-title">${keys.name}</h4>
+                            <p class="card-text">${keys.price}</p>
+                            <p class="card-text">${keys.description}</p>
+                        </div>
+                    </div>
+                </div>`;
+        }
+        document.querySelector(".row").innerHTML = x;
+    });
